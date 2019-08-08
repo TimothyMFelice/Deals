a5e14ee0b6fb636ea91f5c3deeef2200283f6212
TimothyMFelice <timothymfelice@gmail.com>
2019-08-08 14:26:21 -0700

    Init Commit For Project

---
+++ Deals/Deals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deals
+//
+//  Created by Timothy Felice on 8/8/19.
+//  Copyright © 2019 Timothy Felice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
