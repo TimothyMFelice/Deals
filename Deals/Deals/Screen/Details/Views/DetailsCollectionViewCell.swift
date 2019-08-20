51acbc291fda4d04bdd204cb84d43e1d42100315
TimothyMFelice <timothymfelice@gmail.com>
2019-08-19 17:30:40 -0700

    Done

---
+++ Deals/Deals/Screen/Details/Views/DetailsCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  DetailsCollectionViewCell.swift
+//  Deals
+//
+//  Created by Timothy Felice on 8/19/19.
+//  Copyright © 2019 Timothy Felice. All rights reserved.
+//
+
+import UIKit
+
+class DetailsCollectionViewCell: UICollectionViewCell {
+    let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleToFill
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            ])
+    }
+}
